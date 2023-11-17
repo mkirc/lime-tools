379a0cb1336e6de536969b3a395b7e86be697517
mkirc <m.p.kirchner@gmx.de>
2023-11-17 17:03:42 +0100

    introduces some robustness reading FLASH properties

--- convert/flashBlock.py
+++ convert/flashBlock.py
@@ -6,26 +6,38 @@ import numpy as np
 from helper import flatten3DValues
 
 
-class FlashBlockFactory:
+class FlashFactory:
     def __init__(self, flash_file):
         self.file = flash_file
-        self.bb = self.file["bounding box"]
-        self.temperatures = self.file["temp"]
-        self.dusttemperatures = self.file["tdus"]
-        self.vels = (self.file["velx"], self.file["vely"], self.file["velz"])
-        self.mags = (self.file["magx"], self.file["magy"], self.file["magz"])
-        self.densities = self.file["dens"]
+        self.bb = self.file["bounding box"]  # needs no get(), bb is required
+        self.temperatures = self.file.get("temp")
+        self.dusttemperatures = self.file.get("tdus")
+        self.refinementLevels = self.file.get("refine level")
+        self.blockSizes = self.file.get("block size")
+        self.radius = np.sqrt(
+            np.max(self.blockSizes[0]) ** 2
+            + np.max(self.blockSizes[1]) ** 2
+            + np.max(self.blockSizes[2]) ** 2
+        )
+        self.minscale = (
+            np.max(self.blockSizes[0]) / (2 ** (np.max(self.refinementLevels) - 1)) / 8
+        )
+        self.vels = (
+            self.file.get("velx"),
+            self.file.get("vely"),
+            self.file.get("velz"),
+        )
+        self.mags = (
+            self.file.get("magx"),
+            self.file.get("magy"),
+            self.file.get("magz"),
+        )
+        self.densities = self.file.get("dens")
         self.blocks = np.array(self.file["node type"])
         self.leaves = np.where(self.blocks == 1)[0]  # node type == 1 -> leaf
-        self.indices = np.meshgrid(
+        self.gpIndices = np.meshgrid(
             *[range(nib) for nib in self.densities[0].shape], indexing="ij"
         )
-
-    def velocitiesForBlock(self, blockId):
-        return (self.vels[0][blockId], self.vels[1][blockId], self.vels[2][blockId])
-
-    def magfluxesForBlock(self, blockId):
-        return (self.mags[0][blockId], self.mags[1][blockId], self.mags[2][blockId])
 
     def generateBlocksForSlice(self, blockslice):
         for blockId in self.leaves[blockslice]:
@@ -34,28 +46,58 @@ class FlashBlockFactory:
     def createBlock(self, blockId):
         return FlashBlock(
             blockId,
-            self.indices,
+            self.gpIndices,
             self.bb[blockId],
-            self.temperatures[blockId],
-            self.dusttemperatures[blockId],
-            self.densities[blockId],
+            self.temperaturesForBlock(blockId),
+            self.dusttemperaturesForBlock(blockId),
+            self.densitiesForBlock(blockId),
             self.velocitiesForBlock(blockId),
             self.magfluxesForBlock(blockId),
         )
 
+    def temperaturesForBlock(self, blockId):
+        try:
+            return self.temperatures[blockId]
+        except TypeError:
+            return None
+
+    def dusttemperaturesForBlock(self, blockId):
+        try:
+            return self.dusttemperatures[blockId]
+        except TypeError:
+            return None
+
+    def densitiesForBlock(self, blockId):
+        try:
+            return self.densities[blockId]
+        except TypeError:
+            return None
+
+    def velocitiesForBlock(self, blockId):
+        if not self.vels[0]:
+            # raise AttributeError('No velocities present in flashfile.')
+            return None
+        return (self.vels[0][blockId], self.vels[1][blockId], self.vels[2][blockId])
+
+    def magfluxesForBlock(self, blockId):
+        if not self.mags[0]:
+            # raise AttributeError('No mag field present in flashfile.')
+            return None
+        return (self.mags[0][blockId], self.mags[1][blockId], self.mags[2][blockId])
+
 
 class FlashBlock:
-    def __init__(self, blockId, indices, bb, temp, tempdust, dens, vels, mags):
+    def __init__(self, blockId, gpIndices, bb, temp, tempdust, dens, vels, mags):
         self.id = blockId
-        self._Ix, self._Iy, self._Iz = indices
+        self._Ix, self._Iy, self._Iz = gpIndices
 
         self.gridpoints = self.gridpointsForBoundingbox(bb)
-        self.hullpoints = self.hullpoints()
         self.temperatures = self.temperatures(temp)
         self.dusttemperatures = self.dusttemperatures(tempdust)
         self.densities = self.densities(dens)
         self.velocities = self.velocities(vels)
         self.magfluxes = self.magfluxes(mags)
+        self.hullpoints = self.hullpoints()
 
     def gridpointsForBoundingbox(self, bb):
         """takes np.array of shape (3,2) [x,y,z, (upper/lower)]. returns
@@ -73,7 +115,8 @@ class FlashBlock:
 
     def hullpoints(self):
         """hullpoint has either a index of 0 or n[xyz]b. returns bitmask for gridpoints,
-        1 indicating hullpoint, 0 indicating inner point"""
+        1 indicating hullpoint, 0 indicating inner point. This whole method is pretty pointless
+        for anything other than looking at one block"""
         out = []
         dirty_nxb = np.max(self._Ix)  # TODO: this is not generic, refactor.
         gridpointIndices = np.array([self._Ix, self._Iy, self._Iz]).reshape(3, -1).T
@@ -87,16 +130,21 @@ class FlashBlock:
         return out
 
     def temperatures(self, temperatures):
-        return temperatures.flatten()
+        if temperatures is not None:
+            return temperatures.flatten()
 
     def dusttemperatures(self, dusttemperatures):
-        return dusttemperatures.flatten()
+        if dusttemperatures is not None:
+            return dusttemperatures.flatten()
 
     def densities(self, densities):
-        return densities.flatten()
+        if densities is not None:
+            return densities.flatten()
 
     def velocities(self, velocities):
-        return flatten3DValues(velocities[0], velocities[1], velocities[2])
+        if velocities is not None:
+            return flatten3DValues(velocities[0], velocities[1], velocities[2])
 
     def magfluxes(self, magfluxes):
-        return flatten3DValues(magfluxes[0], magfluxes[1], magfluxes[2])
+        if magfluxes is not None:
+            return flatten3DValues(magfluxes[0], magfluxes[1], magfluxes[2])
