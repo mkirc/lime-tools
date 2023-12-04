d66461faa649b6977e6c86a028d8946b7e2217e0
mkirc <m.p.kirchner@gmx.de>
2023-12-04 20:24:03 +0100

    adds test setup for measuring execution time (wip)

--- convert/tests.py
+++ convert/tests.py
@@ -1,51 +1,110 @@
+import time
 import pathlib
+import argparse
+import subprocess
 
+import csv
 import h5py
 import numpy as np
 
 from flashBlock import FlashFactory
-from helper import centerAxis, sampleSphereSurface, radiusForBoundingboxes, sampleSphere
+from helper import (
+    centerAxis,
+    sampleSphereSurface,
+    radiusForBoundingboxes,
+    sampleSphere,
+    createArgumentParser,
+)
 from limeFile import LimeFile
+
+from convert import convertWithArgs
 
 from copy import deepcopy
 
 
+def singleBlockTest():
+    testDir = pathlib.Path(__file__).parent.absolute() / "tests"
+    plotFile0 = testDir.joinpath("in/be_hdf5_plt_cnt_0004")
+    singleBlockOutFile = testDir.joinpath("out/single_test.h5")
+    nSinks = 1000
+    radScale = 1
+    parser = createArgumentParser()
+
+    args = parser.parse_args(
+        [str(plotFile), str(outFile), "-b 1", f"-s {nSinks}", f"-r {radScale}"]
+    )
+    convertWithArgs(args)
+
+
 def allBlocksTest():
     testDir = pathlib.Path(__file__).parent.absolute() / "tests"
-    plotFile = testDir.joinpath("in/be_hdf5_plt_cnt_0102")
+    plotFile = testDir.joinpath("in/be_hdf5_plt_cnt_0004")
     outFile = testDir.joinpath("out/all_test.h5")
     nSinks = 1000
+    radScale = 0.11
+    parser = createArgumentParser()
 
-    with LimeFile(f"{str(outFile)}", "w") as limeFile:
-        flashFile = h5py.File(plotFile, "r")
+    args = parser.parse_args(
+        [str(plotFile), str(outFile), f"-s {nSinks}", f"-r {radScale}"]
+    )
+    convertWithArgs(args)
 
-        ff = FlashFactory(flashFile)
 
-        nBlocks = len(ff.leaves)
-        # nBlocks = 1
-        allLeafSlice = slice(0, nBlocks)
-        radscale = 0.1
+def executionTimeTest():
+    testDir = pathlib.Path(__file__).parent.absolute() / "tests"
+    wipDir = pathlib.Path(__file__).absolute().parent.parent.parent / "wip"
+    plotFile = testDir.joinpath("in/be_hdf5_plt_cnt_0102")
+    outFile = wipDir.joinpath("models/hdf5/nblocks_test.h5")
+    parser = createArgumentParser()
 
-        # print(ff.radius)
-        # print(ff.minscale)
+    for nThreads in range(1, 7):
+        print(f'nThreads: {nThreads}')
 
-        # generate sinkpoints
-        sinkpoints = sampleSphereSurface(nSinks) * ff.radius * radscale
+        csvFilePath = testDir.joinpath(f"out/execT_{nThreads}_threads.csv")
 
-        # prepare outfile
-        limeFile.setupFileAttributes(radius=ff.radius * radscale,
-                                     minscale=ff.minscale)
-        limeFile.setupPrimaryGroups()
-        limeFile.setupPoints(nBlocks=nBlocks, nSinks=nSinks)
+        with open(str(csvFilePath), 'w', newline='') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(['nBlocks', 'convertT', 'execT', 'success'])
 
-        # prepare properties
-        limeFile.setupDensity()
-        limeFile.setupGasTemperature()
-        limeFile.setupVelocity()
+            for nBlocks in range(0, 65, 8):
 
-        # write data
-        limeFile.writeBlocks(ff.generateBlocksForSlice(allLeafSlice))
-        limeFile.writeSinks(sinkpoints)
+                if nBlocks == 0:
+                    nBlocks = 1
+                print(f'nBlocks: {nBlocks}')
+
+                convStart = time.time()
+                args = parser.parse_args(
+                    [
+                        str(plotFile),
+                        str(outFile),
+                        f"-b {nBlocks}",
+                    ]
+                )
+                convertWithArgs(args)
+                convEnd = time.time()
+                convT = convEnd - convStart
+
+                success = False
+                execStart = time.time()
+                limeArgs = ["bash", "hdf5Test.bash"]
+                if nThreads != 1:
+                    limeArgs += ["-p", f"{nThreads}"]
+
+                completedProcess = subprocess.run(
+                    limeArgs,
+                    cwd=str(wipDir),
+                )
+                if completedProcess.returncode == 0:
+                    print("lime run successfully")
+                    success = True
+                else:
+                    print("lime didnt run successfully")
+                execEnd = time.time()
+                execT = execEnd - execStart
+
+                writer.writerow([nBlocks, convT, execT, success])
+            print(f"writing file {str(csvFilePath)}")
+        exit()
 
 
 def threeBlocksTest():
@@ -80,48 +139,12 @@ def threeBlocksTest():
 
         blocks = [block, blockOne, blockTwo]
 
-        limeFile.setupFileAttributes(radius=ff.radius * radScale,
-                                     minscale=ff.minscale)
+        limeFile.setupFileAttributes(radius=ff.radius * radScale, minscale=ff.minscale)
         limeFile.setupPrimaryGroups()
 
         limeFile.setupPoints(nBlocks=len(blocks), nSinks=nSinks)
 
         limeFile.writeBlocks(blocks)
-        limeFile.writeSinks(sinkpoints)
-
-
-def singleBlockTest():
-    testDir = pathlib.Path(__file__).parent.absolute() / "tests"
-    plotFile0 = testDir.joinpath("in/be_hdf5_plt_cnt_0004")
-    singleBlockOutFile = testDir.joinpath("out/single_test.h5")
-    nSinks = 1000
-    radScale = 1
-
-    with LimeFile(f"{str(singleBlockOutFile)}", "w") as limeFile:
-        flashFile = h5py.File(plotFile0, "r")
-
-        ff = FlashFactory(flashFile)
-        print(ff.radius)
-        print(ff.minscale)
-
-        block = next(ff.generateBlocksForSlice(slice(0, 1)))
-
-        block.gridpoints[:, 0] = centerAxis(block.gridpoints[:, 0])
-        block.gridpoints[:, 1] = centerAxis(block.gridpoints[:, 1])
-        block.gridpoints[:, 2] = centerAxis(block.gridpoints[:, 2])
-
-        sinkpoints = sampleSphereSurface(nSinks) * ff.radius * radScale
-
-        limeFile.setupFileAttributes(ff.radius * radScale,
-                                     ff.minscale)
-        limeFile.setupPrimaryGroups()
-        limeFile.setupPoints(nBlocks=1, nSinks=nSinks)
-
-        limeFile.setupDensity()
-        limeFile.setupGasTemperature()
-        limeFile.setupVelocity()
-
-        limeFile.writeBlocks([block])
         limeFile.writeSinks(sinkpoints)
 
 
@@ -172,6 +195,7 @@ def suzanneTest():
 
 
 if __name__ == "__main__":
-    singleBlockTest()
-    allBlocksTest()
+    # singleBlockTest()
+    # allBlocksTest()
     # threeBlocksTest()
+    executionTimeTest()
